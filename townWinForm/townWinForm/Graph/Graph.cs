fa21168feedf0a3846f20e179fb925885a66eccb
Hirotaru <7460624713681YZ$@(((8!>
2015-10-29 23:45:27 +0300

    Added Graph

---
+++ townWinForm/townWinForm/Graph/Graph.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+
+namespace townWinForm.Graph
+{
+    public class Graph
+    {
+        List<Node> Nodes;
+        List<Edge> Edges;
+
+        public Graph()
+        {
+            Nodes = new List<Node>();
+            Edges = new List<Edge>();
+        }
+    }
+}
