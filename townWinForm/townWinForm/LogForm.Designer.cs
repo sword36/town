c666af22ff6fbdacadb562211a94f58a05a5e994
sword36 <sword36rus@gmail.com>
2015-11-07 12:56:37 +0300

    Added scroll to Log textBox

--- townWinForm/townWinForm/LogForm.Designer.cs
+++ townWinForm/townWinForm/LogForm.Designer.cs
@@ -37,6 +37,7 @@
             this.textBox.Location = new System.Drawing.Point(12, 12);
             this.textBox.Multiline = true;
             this.textBox.Name = "textBox";
+            this.textBox.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
             this.textBox.Size = new System.Drawing.Size(231, 237);
             this.textBox.TabIndex = 0;
             // 
